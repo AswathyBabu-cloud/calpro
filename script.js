ff12c6eba32946d6d6329cee12fe05dac494715c
Aswathy Babu <aswathybabu705@gmail.com>
2025-10-07 11:41:15 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,27 @@
+let result = document.getElementById("result");
+
+function appendNumber(number) {
+    result.value += number;
+}
+
+function appendOperator(operator) {
+    if (result.value !== "") {
+        result.value += operator;
+    }
+}
+
+function calculateResult() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = "Error";
+    }
+}
+
+function clearResult() {
+    result.value = "";
+}
+
+function deleteLast() {
+    result.value = result.value.slice(0, -1);
+}
